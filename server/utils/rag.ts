7f90b5be0f308a6fd4577c722c02663da9260118
gienivarnugraha <nivar.nugraha@gmail.com>
2025-08-20 09:24:19 +0700

    fix document path

--- server/utils/rag.ts
+++ server/utils/rag.ts
@@ -53,10 +53,10 @@ const createDocumentRetrievalChain = async () => {
     // const __dirname = fileURLToPath(new URL('.', import.meta.url));
     // const publicDir = resolve(__dirname, '../../public/documents/'); // Adjust path based on your file structure
     // const imagePath = join(publicDir, 'document.pdf');
-    const dir = join(process.cwd(), '..', '..');
+    const dir = join(process.cwd(), '..');
     console.warn(await readdir(dir))
 
-    const imagePath = join(process.cwd(), '../../public/documents/document.pdf');
+    const imagePath = join(process.cwd(), '../documents/document.pdf');
     console.warn(resolve(imagePath))
     console.warn(await readFile(imagePath))
 
